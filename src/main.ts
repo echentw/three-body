053a4800d944847b608e4c666e50167c1a11c57c
Eric Chen <ericchen098@gmail.com>
2019-01-16 00:17:08 -0800

    show velocities and accelerations

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ type Vector = {
 
 type Point = Vector;
 type Velocity = Vector;
+type Acceleration = Vector;
 
 type PlanetSpecs = {
   radius: number;
@@ -26,12 +27,14 @@ class Planet {
   public readonly mass: number;
   public position: Point;
   public velocity: Velocity;
+  public acceleration: Acceleration;
 
   constructor(specs: PlanetSpecs) {
     this.position = specs.position;
     this.radius = specs.radius;
     this.mass = specs.mass;
     this.velocity = specs.velocity;
+    this.acceleration = { x: 0, y: 0 };
   }
 
   // Updates this planet's position given the other planets' positions and masses.
@@ -53,12 +56,14 @@ class Planet {
       netForce.y += force * direction.y;
     });
 
-    const netAcceleration: Vector = {
+    const netAcceleration: Acceleration = {
       x: netForce.x / this.mass,
       y: netForce.y / this.mass,
     };
 
     const dt = 0.1;
+
+    this.acceleration = netAcceleration;
 
     this.velocity.x += netAcceleration.x * dt;
     this.velocity.y += netAcceleration.y * dt;
@@ -67,71 +72,83 @@ class Planet {
     this.position.y += this.velocity.y * dt;
   }
 
-  drawVelocityVector() {
+  getKonvaVelocityVector(): Konva.Shape {
+    return new Konva.Arrow({
+      x: this.position.x,
+      y: this.position.y,
+      points: [0, 0, this.velocity.x * 2, this.velocity.y * 2],
+      pointerLength: 8,
+      pointerWidth : 6,
+      fill: 'white',
+      stroke: 'white',
+      strokeWidth: 4,
+    });
   }
 
-  drawAccelerationVector() {
+  getKonvaAccelerationVector(): Konva.Shape {
+    return new Konva.Arrow({
+      x: this.position.x,
+      y: this.position.y,
+      points: [0, 0, this.acceleration.x * 4, this.acceleration.y * 4],
+      pointerLength: 8,
+      pointerWidth : 6,
+      fill: 'blue',
+      stroke: 'blue',
+      strokeWidth: 4,
+    });
   }
 
-  getGroup() {
-    if (flags.showVelocities) {
-      this.drawVelocityVector();
-    }
-
-    if (flags.showAccelerations) {
-      this.drawAccelerationVector();
-    }
-
-    const shape = new Konva.Circle({
+  getKonvaGroup(): Konva.Group {
+    const group = new Konva.Group();
+    group.add(new Konva.Circle({
       fill: 'green',
       radius: this.radius,
       x: this.position.x,
       y: this.position.y,
-    });
-
-    return shape;
+    }));
+    return group;
   }
 }
 
 function main() {
-  const planet = new Planet({
-    radius: 10.0,
-    mass: 1.0,
-    position: {
-      x: 300.0,
-      y: 300.0,
-    },
-    velocity: {
-      x: -2.0,
-      y: -1.0,
-    },
-  });
-
-  const planet2 = new Planet({
-    radius: 20.0,
-    mass: 10.0,
-    position: {
-      x: 400.0,
-      y: 300.0,
-    },
-    velocity: {
-      x: 0.0,
-      y: 20.0,
-    },
-  });
-
-  const planet3 = new Planet({
-    radius: 20.0,
-    mass: 10.0,
-    position: {
-      x: 600.0,
-      y: 300.0,
-    },
-    velocity: {
-      x: 0.0,
-      y: -20.0,
-    },
-  });
+  const planets = [
+    new Planet({
+      radius: 10.0,
+      mass: 1.0,
+      position: {
+        x: 300.0,
+        y: 300.0,
+      },
+      velocity: {
+        x: -2.0,
+        y: -1.0,
+      },
+    }),
+    new Planet({
+      radius: 20.0,
+      mass: 10.0,
+      position: {
+        x: 400.0,
+        y: 300.0,
+      },
+      velocity: {
+        x: 0.0,
+        y: 20.0,
+      },
+    }),
+    new Planet({
+      radius: 20.0,
+      mass: 10.0,
+      position: {
+        x: 600.0,
+        y: 300.0,
+      },
+      velocity: {
+        x: 0.0,
+        y: -20.0,
+      },
+    }),
+  ];
 
   const stage = new Konva.Stage({
     container: 'container',
@@ -141,28 +158,40 @@ function main() {
 
   const backgroundLayer = new Konva.Layer();
   backgroundLayer.add(new Konva.Rect({
-    width: 1000,
-    height: 600,
+    width: stage.getWidth(),
+    height: stage.getHeight(),
     fill: 'black',
   }));
 
-  const layer = new Konva.Layer();
+  const mainLayer = new Konva.Layer();
+  const vectorsLayer = new Konva.Layer();
 
   function update() {
-    planet.update([planet2, planet3]);
-    planet2.update([planet, planet3]);
-    planet3.update([planet, planet2]);
+    // Update the representation of the planet objects.
+    planets.forEach(planet => {
+      const otherPlanets = planets.filter(otherPlanet => otherPlanet !== planet);
+      planet.update(otherPlanets);
+    });
 
+    // Clear the canvas
     stage.removeChildren();
-    layer.removeChildren();
+    mainLayer.removeChildren();
+    vectorsLayer.removeChildren();
 
+    planets.forEach(planet => mainLayer.add(planet.getKonvaGroup()));
+
+    // Optional flags
+    if (flags.showVelocities) {
+      planets.forEach(planet => vectorsLayer.add(planet.getKonvaVelocityVector()));
+    }
+    if (flags.showAccelerations) {
+      planets.forEach(planet => vectorsLayer.add(planet.getKonvaAccelerationVector()));
+    }
+
+    // Add the layers to the stage
     stage.add(backgroundLayer);
-
-    layer.add(planet.getGroup());
-    layer.add(planet2.getGroup());
-    layer.add(planet3.getGroup());
-
-    stage.add(layer);
+    stage.add(mainLayer);
+    stage.add(vectorsLayer);
 
     requestAnimationFrame(update);
   }
